68a46942cfa02029824574006baa5cfc7af29442
Joshua Park <wmpark@knowre.com>
2020-09-21 13:25:38 +0900

    Add: MainActivity - top portion UI

--- app/src/main/java/com/example/asyncexamples/presentation/MainActivity.kt
+++ app/src/main/java/com/example/asyncexamples/presentation/MainActivity.kt
@@ -2,14 +2,28 @@ package com.example.asyncexamples.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelStore
+import androidx.lifecycle.ViewModelStoreOwner
 import com.example.asyncexamples.R
+import com.example.asyncexamples.application.BaseViewModel
+import com.example.asyncexamples.application.CallbackViewModel
+import javax.security.auth.callback.Callback
 
 class MainActivity : AppCompatActivity() {
+
+    // INHERITED
 
     override
     fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    // PRIVATE
+
+    // TODO: Use DI
+    private val viewModel: BaseViewModel = ViewModelProvider(this)
+        .get(CallbackViewModel::class.java)
 
 }
