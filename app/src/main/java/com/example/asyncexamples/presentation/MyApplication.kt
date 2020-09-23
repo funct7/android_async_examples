6283d6dc0ff426e9595407c325e09184c88a0091
Josh Woomin Park <joshuaparkwm@gmail.com>
2020-09-23 11:36:09 +0900

    Add: MyApplication mock responses

--- app/src/main/java/com/example/asyncexamples/presentation/MyApplication.kt
+++ app/src/main/java/com/example/asyncexamples/presentation/MyApplication.kt
@@ -1,17 +1,21 @@
 package com.example.asyncexamples.presentation
 
 import android.app.Application
+import android.graphics.BitmapFactory
 import android.net.Uri
+import com.example.asyncexamples.R
+import com.example.asyncexamples.application.repository.ImageRepositoryImpl
+import com.example.asyncexamples.application.repository.PostRepositoryImpl
 import com.example.asyncexamples.application.repository.UserRepositoryImpl
+import com.example.asyncexamples.domain.entity.PostModel
 import com.example.asyncexamples.domain.entity.UserModel
 import java.util.*
 
 class MyApplication : Application() {
 
-    override
-    fun onCreate() {
-        super.onCreate()
+    // INTERFACE
 
+    fun mockSuccess() {
         val user = object : UserModel {
             override val id: String = UUID.randomUUID().toString()
             override val username: String = "Jim Halpert"
@@ -25,7 +29,48 @@ class MyApplication : Application() {
         }
 
         UserRepositoryImpl.returnTable[UserRepositoryImpl::signIn.name] = user
-        UserRepositoryImpl.returnTable[UserRepositoryImpl::fetchUser.name] = otherUser
+        UserRepositoryImpl.returnTable[UserRepositoryImpl::fetchUser.name] = mapOf(
+            user.id to user,
+            otherUser.id to otherUser
+        )
+        ImageRepositoryImpl.returnTable[ImageRepositoryImpl::resolveImage.name] = mapOf(
+            user.pictureUrl to BitmapFactory.decodeResource(resources, R.drawable.jim),
+            otherUser.pictureUrl to BitmapFactory.decodeResource(resources, R.drawable.dwight)
+        )
+        PostRepositoryImpl.returnTable[PostRepositoryImpl::fetchPost.name] = listOf(
+            object : PostModel {
+                override val id: String = UUID.randomUUID().toString()
+                override val userId: String = otherUser.id
+                override val content: String = "random stuff"
+            },
+            object : PostModel {
+                override val id: String = UUID.randomUUID().toString()
+                override val userId: String = user.id
+                override val content: String = "anything"
+            },
+            object : PostModel {
+                override val id: String = UUID.randomUUID().toString()
+                override val userId: String = otherUser.id
+                override val content: String = "foo bar baz"
+            },
+            object : PostModel {
+                override val id: String = UUID.randomUUID().toString()
+                override val userId: String = otherUser.id
+                override val content: String = "yucky"
+            }
+        )
+    }
+
+    fun mockFailure() {
+        UserRepositoryImpl.returnTable[UserRepositoryImpl::signIn.name] = Throwable()
+    }
+
+    // INHERITED
+
+    override
+    fun onCreate() {
+        super.onCreate()
+        mockSuccess()
     }
 
 }
